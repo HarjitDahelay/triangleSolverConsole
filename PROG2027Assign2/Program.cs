cfbc1f8c16f6e3e5926a1c176af7f87008c1116f
Harjit Dahelay <harjitdahelay@gmail.com>
2020-02-24 12:16:39 -0500

    completed console app

--- PROG2027Assign2/Program.cs
+++ PROG2027Assign2/Program.cs
@@ -10,6 +10,53 @@ namespace PROG2027Assign2
     {
         static void Main(string[] args)
         {
+            bool value = true;
+
+            do
+            {
+                Console.WriteLine("Select any option: ");
+                Console.WriteLine("1.Enter Triangle Dimensions");
+                Console.WriteLine("2.Exit");
+
+                string userOption = Console.ReadLine();
+
+                if (userOption == "1")
+                {
+                    value = true;
+                    do
+                    {
+                        Console.WriteLine("\nEnter 3 sides of triangle:");
+                        try
+                        {
+                            int side1 = Convert.ToInt32(Console.ReadLine());
+                            int side2 = Convert.ToInt32(Console.ReadLine());
+                            int side3 = Convert.ToInt32(Console.ReadLine());
+
+                            if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+                            {
+                                Console.WriteLine("The side of triangle cannot be less than or equal to zero.");
+                            }
+                            value = false;
+
+                            Console.WriteLine(TriangleSolver.Analyze(side1, side2, side3));
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("ERROR: Please Try Again " + e.Message);
+                        }
+                    } while (value);
+                    value = true;
+                }
+                else if (userOption == "2")
+                {
+                    value = false;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Input,Choose Option Again\n");
+                }
+            } while (value);
         }
     }
 }
+
---
+++ PROG2027Assign2/TriangleSolver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG2027Assign2
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int a, int b, int c)
+        {
+            string output = "";
+
+           
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                output = "Not valid traingle";
+            }
+            else if (a == b && b == c && b == a)
+            {
+                output += "This is a valid Equilateral triangle";
+
+            }
+            else if (a == b || b == c || b == a)
+            {
+                output += "This is a valid Isosceles triangle";
+
+            }
+            else
+            {
+                output += "This is Scalene triangle";
+
+            }
+           
+
+            return output;
+
+        }
+    }
+}
